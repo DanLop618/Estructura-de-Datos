8338d96256ffc04a9fa96cf2550c564c63ace0f3
danlop__ <oscarlp618@gmail.com>
2022-10-28 08:51:14 -0500

    Documentación actualizada

--- structures/Binodo.py
+++ structures/Binodo.py
@@ -4,14 +4,10 @@ class Nodo():
     """Inicializa un nuevo Binodo de datos.
     @type dato: any
     @param dato: El dato que éste nodo almacenará
-    @type siguiente: Nodo || None
-    @param siguiente: El binodo siguiete al cual apunta éste binodo
-    @type siguiente: Nodo || None
-    @param siguiente: El binodo anterior al que apunta éste binodo
     @rtype: Binodo
     @returns: El binodo creado
     """
-    def __init__( self, dato, siguiente, anterior ):
+    def __init__( self, dato ):
         self._dato      = dato;
-        self._siguiente = siguiente;
-        self._anterior  = anterior;
+        self._siguiente = None;
+        self._anterior  = None;
